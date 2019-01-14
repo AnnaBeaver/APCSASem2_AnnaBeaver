4e5d89af4d621c6ac2882d2c5a0b38977bf9f0fc
Anna <793308@seq.org>
2019-01-14 09:36:20 -0800

    started 114

---
+++ Lab 114 - 3 Algortihms with metrics/Main.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    public static void main(){
+        int[] list = new int[7];
+        
+        
+        Sort.setUp(list);
+        Sort.mySort(list);
+        Sort.print(list);
+        System.out.println();
+        
+        Sort.setUp(list);
+        Sort.bubbleSort(list);
+        Sort.print(list);
+        System.out.println();
+        
+        
+        Sort.setUp(list);
+        Sort.selectionSort(list);
+        Sort.print(list);
+    }
+}
---
+++ Lab 114 - 3 Algortihms with metrics/Sort.java
@@ -0,0 +1,80 @@
+
+/**
+ * Homework assignment for January 9, 2019
+ * Create a sorting algorithm for a list of numbers
+ * Anna Beaver
+ */
+public class Sort
+{
+    //setting up the array with random values
+    public static void setUp(int list[]){
+        //setting the array up
+        for(int y = 0; y<list.length;y++){
+            //sets each value of the array to a random integer between 1 and 10
+            list[y] = (int) (Math.random()*10)+1;
+        }
+    }
+    
+    //sorting the array so that the smallest numbers are first
+    public static void mySort(int list[]){
+        //traverse the array
+        for(int n = 0; n < list.length; n++){
+            //traverse the array again
+            for(int a = 0; a < list.length-1; a++){
+                /** by traversing the array twice, I can compare every value to every other value */
+                //if the original value is greater than the value after it in the array
+                if(list[a] < list[a+1]){
+                    //set temporary variables equal to the original values in the array
+                    int i = list[a];
+                    int j =list[a+1];
+
+                    //switch the values in  the array by setting them equal to the other value 
+                    //from the top bit of this if statement
+                    list[a+1] = i;
+                    list[a] = j;
+                }
+            }
+        }
+    }
+    
+    public static void bubbleSort(int list[]){
+        //traverse the array
+        //making it go from last to first maked the limit
+        for(int n = list.length; n >0; n--){
+            //traverse the array again
+            for(int a = 0; a < list.length-1; a++){
+                //if the current value is smaller than the value of the one to it's right
+                if(list[a]>list[a+1]){
+                    //swap them
+                    int x = list[a+1];
+                    list[a+1] = list[a];
+                    list[a]=x;
+                }
+            }
+            }
+        }
+    
+    
+    public static void selectionSort(int list[]){
+        for(int n = 0; n < list.length; n++){
+            for(int a = 0; a < list.length-1; a++){
+                /** by traversing the array twice, I can compare every value to every other value */
+                //if the original value is greater than the value after it in the array
+                if(list[n] > list[a]){
+                    int x = list[n];
+                    list[n] = list[a];
+                    list[a]=x;
+                }
+            }
+        }
+    }
+    
+    //printing each value of the array on its own line
+    public static void print(int list[]){
+        //traverse the array
+        for(int x = 0; x<list.length; x++){
+            //print each value of the array
+            System.out.println(list[x]);
+        }
+    }
+}
