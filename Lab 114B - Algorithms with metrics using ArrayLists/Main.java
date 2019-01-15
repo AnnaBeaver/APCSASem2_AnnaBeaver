2a62c5b693337824598ea55c9519a169d8e30844
Anna <793308@seq.org>
2019-01-14 18:42:52 -0800

    done!

---
+++ Lab 114B - Algorithms with metrics using ArrayLists/Main.java
@@ -0,0 +1,64 @@
+//import java.lang.System.nanoTime();
+import java.lang.*;
+import java.util.*;
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    public static void main(){
+        
+        System.out.println();
+        System.out.println("My Sort");
+        ArrayList <Integer> list1 = new ArrayList<Integer>();
+        Sort.setUp(list1);
+        long start1 = Sort.startTimer();
+        Sort.mySort(list1);
+        long end1 = Sort.endTimer();
+        Sort.displayTime(start1, end1);
+        Sort.print(list1);
+        System.out.println();
+        System.out.println();
+        
+        System.out.println();
+        System.out.println("Bubble Sort");
+        ArrayList <Integer> list2 = new ArrayList<Integer>();
+        Sort.setUp(list2);
+        long start2 = Sort.startTimer();
+        Sort.bubbleSort(list2);
+        long end2 = Sort.endTimer();
+        Sort.displayTime(start2, end2);
+        Sort.print(list2);
+        System.out.println();
+        System.out.println();
+        
+        System.out.println();
+        System.out.println("Insertion Sort");
+        ArrayList <Integer> list3 = new ArrayList<Integer>();
+        Sort.setUp(list3);
+        long start3 = Sort.startTimer();
+        Sort.insertionSort(list3);
+        long end3 = Sort.endTimer();
+        Sort.displayTime(start3, end3);
+        Sort.print(list3);
+        System.out.println();
+        System.out.println();
+        
+        System.out.println();
+        System.out.println("Selection Sort");
+        ArrayList <Integer> list4 = new ArrayList<Integer>();
+        Sort.setUp(list4);
+        long start4 = Sort.startTimer();
+        Sort.selectionSort(list4);
+        long end4 = Sort.endTimer();
+        Sort.displayTime(start4, end4);
+        Sort.print(list4);
+        System.out.println();
+        System.out.println();
+        
+    }
+}  
+
---
+++ Lab 114B - Algorithms with metrics using ArrayLists/Sort.java
@@ -0,0 +1,147 @@
+import java.util.*;
+/**
+ * Homework assignment for January 9, 2019
+ * Create a sorting algorithm for a list of numbers
+ * Anna Beaver
+ */
+public class Sort
+{
+    //setting up the array with random values
+    public static void setUp(ArrayList <Integer> list){
+        //setting the array up
+        for(int y = 0; y <= 7;y++){
+            //sets each value of the array to a random integer between 1 and 10
+            list.add ((int) (Math.random()*10)+1);
+        }
+    }
+
+    //sorting the array so that the smallest numbers are first
+    public static void mySort(ArrayList <Integer> list){
+        int swapCounter = 0;
+        int compareCounter = 0;
+        //traverse the array
+        for(int n = 0; n < list.size(); n++){
+            //traverse the array again
+            for(int a = 1; a < list.size(); a++){
+                /** by traversing the array twice, I can compare every value to every other value */
+                //if the original value is greater than the next value in the array
+                compareCounter++;
+                if(list.get(a) < list.get(a-1)){
+                    //set temporary variables equal to the original values in the array
+                    int i = list.get(a);
+                    int j =list.get(a-1);
+
+                    //switch the values in  the array by setting them equal to the other value 
+                    //from the top bit of this if statement
+                    list.set(a-1,i);
+                    list.set(a,j);
+                    
+                    swapCounter++;
+                }
+            }
+        }
+        System.out.println("Swap Counter = " + swapCounter);
+        System.out.println("Compare Counter = " + compareCounter);
+    }
+
+    public static void bubbleSort(ArrayList <Integer> list){
+        int swapCounter = 0;
+        int compareCounter = 0;
+        //traverse the array
+        //making it go from last to first maked the limit
+        for(int n = list.size(); n >0; n--){
+            //traverse the array again
+            for(int a = 0; a < list.size()-1; a++){
+                //if the current value is smaller than the value of the one to it's right
+                compareCounter++;
+                if(list.get(a)>list.get(a+1)){
+                    //swap them
+                    int x = list.get(a+1);
+                    int y = list.get(a);
+                    list.set(a+1,y);
+                    list.set(a,x);
+                    
+                    swapCounter++;
+                }
+            }
+        }
+        System.out.println("Swap Counter = " + swapCounter);
+        System.out.println("Compare Counter = " + compareCounter);
+    }
+
+    public static void insertionSort(ArrayList <Integer> list){
+        //create a temporary variable
+        int test;
+        int swapCounter = 0;
+        int compareCounter = 0;
+        //traverse the array starting at 1
+        for(int n = 1; n < list.size(); n++){
+            //traverse the array starting at the end
+            for(int a = n; a > 0; a--){
+                //if the current value is greater than the one to its left
+                compareCounter++;
+                if(list.get(a) < list.get(a-1)){
+                    //swap them
+                    test = list.get(a);
+                    int test2 = list.get(a-1);
+                    list.set(a,test2);
+                    list.set(a-1,test);
+                    
+                    swapCounter++;
+                }
+            }
+        }
+        System.out.println("Swap Counter = " + swapCounter);
+        System.out.println("Compare Counter = " + compareCounter);
+    }
+
+    public static void selectionSort(ArrayList <Integer> list){
+        int swapCounter = 0;
+        int compareCounter = 0;
+        for(int n = 0; n < list.size(); n++){
+            for(int a = 0; a < list.size(); a++){
+                /** by traversing the array twice, I can compare every value to every other value */
+                //if the original value is greater than the value after it in the array
+                compareCounter++;
+                if(list.get(n) < list.get(a)){
+                    int x = list.get(n);
+                    list.set(n,list.get(a) );
+                    list.set(a,x);
+                    
+                    swapCounter++;
+                }
+            }
+        }
+        System.out.println("Swap Counter = " + swapCounter);
+        System.out.println("Compare Counter = " + compareCounter);
+    }
+    //printing each value of the array on its own line
+    public static void print(ArrayList <Integer> list){
+        //traverse the array
+        for(int x = 0; x<list.size(); x++){
+            //print each value of the array
+            System.out.println(list.get(x));
+        }
+    }
+
+    public static long startTimer(){
+        long start = System.nanoTime();
+        return start;
+    }
+
+    public static long endTimer(){
+        long end = System.nanoTime();
+        return end;
+    }
+
+    public static void displayTime(long start, long end){
+        long display = end-start;
+        System.out.print("Time = " + display);
+    }
+
+    public static int swapCounter(){
+        int count = 0;
+
+        return count;
+    }
+}
